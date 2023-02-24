8c975689dfca14bbcf598a931471081ab167a35a
hadbrodad94 <101060747+hadbrodad94@users.noreply.github.com>
2023-02-23 22:14:00 -0500

    Add files via upload

---
+++ Recursive Lab/ArraySum.java
@@ -0,0 +1,13 @@
+
+public class ArraySum {
+	//recursive method
+	public int sumOfArray(Integer [] a, int index) {
+		int sum;
+		if(index == 0) {
+			sum = a[index];
+		}else {
+			sum = sumOfArray(a,index-1) + a[index];
+		}
+		return sum;
+	}
+}
